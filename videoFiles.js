880c0d52be56ca5d18c34c0a3546cf3aa0a181f1
Tsaiiiii <lillian89915@gmail.com>
2024-11-05 12:32:02 +0800

    add vtt file for video11

--- videoFiles.js
+++ videoFiles.js
@@ -62,5 +62,41 @@ const videoFiles = {
                 { "start": 78.8 , "end": 79.522 }
             ]
         }
+    },
+    "Lesson 11: Come Over to My Place": {
+        "src": "mp4/video11.mp4",
+        "audioSrc": "wav/11_wav.wav",
+        "subtitlesSrc": {
+            "Anna": "subtitles/11_A.vtt",
+            "Marsha": "subtitles/11_B.vtt",
+            "default": "subtitles/11_practice.vtt"
+        },
+        "muteSegments": {
+            "Anna": [
+                { "start": 1.0 , "end": 16.725 },
+                { "start": 20.577 , "end": 21.161 },
+                { "start": 24.505 , "end": 27.605 },
+                { "start": 34.226 , "end": 44.165 },
+                { "start": 51.841 , "end": 63.015 },
+                { "start": 64.599 , "end": 67.431 },
+                { "start": 70.095 , "end": 82.577 },
+                { "start": 85.457 , "end": 89.505 },
+                { "start": 92.265 , "end": 100.685 },
+                { "start": 107.545 , "end": 108.485 },
+                { "start": 113.785 , "end": 123.585 }
+            ],
+            "Marsha": [
+                { "start": 17.065 , "end": 20.449 },
+                { "start": 21.273 , "end": 22.165 },
+                { "start": 30.025 , "end": 33.566 },
+                { "start": 44.745 , "end": 51.697 },
+                { "start": 63.095 , "end": 64.423 },
+                { "start": 67.623 , "end": 69.475 },
+                { "start": 82.761 , "end": 85.329 },
+                { "start": 89.625 , "end": 91.325 },
+                { "start": 105.585 , "end": 107.205 },
+                { "start": 109.705 , "end": 110.885 }
+            ]
+        }
     }
 }
